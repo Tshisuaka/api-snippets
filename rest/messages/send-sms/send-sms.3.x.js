3ae8d4dbd548f78234bafce9d7e6b0f15eaa9eab
Tshisuaka <73997170+Tshisuaka@users.noreply.github.com>
2020-11-05 12:56:58 +0100

    Update send-sms.3.x.js

--- rest/messages/send-sms/send-sms.3.x.js
+++ rest/messages/send-sms/send-sms.3.x.js
@@ -1,14 +1,14 @@
 // Twilio Credentials
-const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-const authToken = 'your_auth_token';
+const accountSid = 'ACf4b3e183d4a765541c213a0aff06aca1';
+const authToken = 'f2cbdc903a9f8f83a5b9f042675bfd1d';
 
 // require the Twilio module and create a REST client
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-    to: '+15558675310',
+    to: '+243 815 507 882',
     from: '+15017122661',
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    body: 'ceci est un message d'essai envoyer par Daniel Tshisuaka',
   })
   .then(message => console.log(message.sid));
